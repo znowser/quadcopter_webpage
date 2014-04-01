22eb67a02dfa7660543c911ccc641abd20264f9d
marno512 <marno512@student.liu.se>
2014-04-01 18:56:42 +0200

    Website getting started

---
+++ QuadFace/QuadFace/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'QuadFace.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+	url(r'^stream/', include('VideoStream.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
