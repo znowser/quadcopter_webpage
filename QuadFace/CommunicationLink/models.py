aaa33e8deab3aabe3e2f1ffb11a7775d83e7107d
marno512 <marno512@student.liu.se>
2014-05-05 18:49:21 +0200

    small fixes

--- QuadFace/CommunicationLink/models.py
+++ QuadFace/CommunicationLink/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 # Create your models here.
 class QuadCopterData(models.Model):
-    x_angle = models.IntegerField(default=0)
-    y_angle = models.IntegerField(default=0)
-    z_angle = models.IntegerField(default=0)
+    BatteryCell1 = models.IntegerField(default=0)
+    BatteryCell2 = models.IntegerField(default=0)
+    BatteryCell3 = models.IntegerField(default=0)
+
+    Engine1 = models.IntegerField(default=0)
+    Engine2 = models.IntegerField(default=0)
+    Engine3 = models.IntegerField(default=0)
+    Engine4 = models.IntegerField(default=0)
+
+    Temperature = models.DecimalField(max_digits=4,decimal_places=1)#Celsius
+    Altitude = models.DecimalField(max_digits=5,decimal_places=1)#Meter
+
+    Roll = models.DecimalField(max_digits=4,decimal_places=1)
+    Pitch = models.DecimalField(max_digits=4,decimal_places=1)
+    Yaw = models.DecimalField(max_digits=4,decimal_places=1)
+    
     
