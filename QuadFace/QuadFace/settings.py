69b5fb57438f17429a75ce5d7f29586c668f6801
ramboerik <eriktideman@gmail.com>
2014-05-05 11:33:52 +0200

    fixade indentering

--- QuadFace/QuadFace/settings.py
+++ QuadFace/QuadFace/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'ws4redis',
     'VideoStream',
     'CommunicationLink',
-	'AuthSystem',
+    'AuthSystem',
     'client',
     'maps',
 )
--- QuadFace/QuadFace/urls.py
+++ QuadFace/QuadFace/urls.py
@@ -5,9 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', include('client.urls')),
-	url(r'^stream/', include('VideoStream.urls')),
+    url(r'^stream/', include('VideoStream.urls')),
     url(r'^maps/', include('maps.urls')),
-	url(r'^communication/', include('CommunicationLink.urls')),
-	url(r'^auth/', include('AuthSystem.urls')),
+    url(r'^communication/', include('CommunicationLink.urls')),
+    url(r'^auth/', include('AuthSystem.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
